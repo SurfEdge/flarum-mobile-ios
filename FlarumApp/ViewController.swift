a7503ff8676d380efa0b37a698aa7dc3981b6e32
Chamath Palihawadana <chamathpali123@gmail.com>
2017-05-24 18:26:17 +0530

    Initial Commit

---
+++ FlarumApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlarumApp
+//
+//  Created by Chamath Palihawadana on 5/24/17.
+//  Copyright © 2017 Chamath Palihawadana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
